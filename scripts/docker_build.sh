8f951157fa0696bd777511ccde12ab208a853a1f
ilarimikkonen <ilari.mikkonen@apinf.io>
2019-04-30 14:03:46 +0300

    Create docker_build.sh
    initial version of docker_build.sh

---
+++ scripts/docker_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Copyright 2019 Apinf Oy
+#This file is covered by the EUPL license.
+#You may obtain a copy of the licence at
+#https://joinup.ec.europa.eu/community/eupl/og_page/european-union-public-licence-eupl-v11
+
+set -ev
+
+docker build -t apinf/apinf-umbrella:$DOCKER_TAG .
+
+if [ "${TRAVIS_PULL_REQUEST}" = "false" -a "${TRAVIS_REPO_SLUG}" = "apinf/apinf-umbrella" ]
+then
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+#docker push apinf/platform:$DOCKER_TAG
+fi
